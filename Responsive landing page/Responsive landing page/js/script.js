06de3493e842f1b58e70d64dd44fa1992794198e
thedrishtiwari <161928458+thedrishtiwari@users.noreply.github.com>
2024-03-02 14:16:40 +0530

    Add files via upload

---
+++ Responsive landing page/Responsive landing page/js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  });
+  
